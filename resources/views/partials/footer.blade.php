3f4d2b86b68bf6240b629737ea8a15455720ad60
James King <ripixel@gmail.com>
2015-08-11 21:35:32 +0100

    Removing Laravel nonsense for mockup demo :peach:

--- resources/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-    <div class="container">
-        <p>Copyright &copy; James King, <a href="http://ripixel.co.uk">ripixel.co.uk</a></p>
-    </div>
-</div>
--- resources/views/partials/nav.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="navbar center">
-    <div class="container">
-        <a href="#">Home</a> <a href="#">News</a> <a href="#" class="home-a">&nbsp;</a> <a href="#">Classes</a> <a href="#">Login</a>
-    </div>
-</div>
