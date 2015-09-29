73acb54423495fb36590fa84817bfade9249966d
James King <ripixel@gmail.com>
2015-09-29 09:53:26 +0100

    Fixed picture_link not being fillable for User model :fried_shrimp:

--- app/User.php
+++ app/User.php
@@ -25,7 +25,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      *
      * @var array
      */
-    protected $fillable = ['first_name', 'last_name', 'email', 'password', 'admin', 'member', 'picture_url', 'email_confirmed'];
+    protected $fillable = ['first_name', 'last_name', 'email', 'password', 'admin', 'member', 'picture_link', 'email_confirmed'];
 
     /**
      * The attributes excluded from the model's JSON form.
