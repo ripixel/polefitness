83acaa9456ef8cdc30dac0ba12dd2a60be885d05
James King <ripixel@gmail.com>
2016-04-22 00:41:49 +0100

    Added semi-colon at end of email dump

--- resources/views/users/emaildump.blade.php
+++ resources/views/users/emaildump.blade.php
@@ -7,11 +7,11 @@
 @section('content')
 	<h1>User Manager</h1>
 	<h2>Showing Confirmed Emails</h2>
-	
+
 	@include('users.actions')
-	
+
 	<div style="margin-top: 8px;"></div>
 	@foreach($emails as $email)
-		{{ $email }}<br/>
+		{{ $email }};<br/>
 	@endforeach
 @endsection
